cd39c76e9f15e7b1b18419d26896a3cc3e4bf67e
chenquanwei <1621357686@qq.com>
2020-01-27 21:22:02 +0800

    git commit -m 这是试验文件夹里面的东西

---
+++ 试验/011移动端字体大小测试/rem.js
@@ -0,0 +1,19 @@
+(function(win,doc){
+    var docEl=doc.documentElement,
+        deviceEvt='orientationchange' in window ? 'orientationchange' : 'resize';
+    recalc = function(){
+        var clientWidth= docEl.clientWidth > 750 ? 750 : docEl.clientWidth;
+        if(!clientWidth) return;
+        docEl.style.fontSize = 100 * (clientWidth / 750) + "px"
+    }
+    if(! win.addEventListener) return;
+    win.addEventListener('deviceEvt',recalc,false)
+    doc.addEventListener('DOMContentLoaded',recalc,false)
+})(window,document);
+$(document).ready(function () {
+    var wid = $(window).width();
+    var hei = 2471 * wid / 750;
+
+    $('.content').css('height',hei+'px');
+})
+
